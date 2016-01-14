1afa668d5866e63cc5d9720e207af812675a94e8
malim360 <manuarelun@hotmail.com>
2016-01-14 09:32:44 +0100

    add ideas-json-module

---
+++ README.md
@@ -0,0 +1,12 @@
+# IDEAS-JSON-MODULE
+
+IDEAS-JSON-MODULE is a web project that provides a languague for ideas-studio.
+
+##JSON
+
+Let change to YAML.
+
+## Requirements
+
+Mandatory requirements:
+* [Java SE Development Kit 7 or newer](http://www.oracle.com/technetwork/es/java/javase/downloads/index.html)
---
+++ languages/ideas-json-language/src/main/java/es/us/isa/ideas/controller/json/JsonTestController.java
@@ -0,0 +1,22 @@
+package es.us.isa.ideas.controller.json;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import es.us.isa.ideas.module.controller.BaseTestModuleController;
+
+
+/**
+ * Applied Software Engineering Research Group (ISA Group) University of
+ * Sevilla, Spain
+ *
+ * @author Manuel Arenillas <marenillas@us.es>
+ * @version 1.0
+ */
+
+
+@Controller
+@RequestMapping("/tests")
+public class JsonTestController extends BaseTestModuleController {
+		
+}
